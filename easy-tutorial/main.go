47ddea7717d915e4a21bc1ba39c3873dbffaed13
zero.li <zeroliql@gmail.com>
2023-08-29 17:09:39 -0700

    local var init

--- easy-tutorial/main.go
+++ easy-tutorial/main.go
@@ -4,5 +4,9 @@ import "fmt"
 
 func main() {
   power := 9000
+  // compiler error
+  //power := 9001
+  x, power := 1, 8000  // ok, since x is new
+  fmt.Println("x = ", x)
   fmt.Printf("it's over %d\n", power)
 }
