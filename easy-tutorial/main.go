f69164f4acc2f42282eb51871d13373c250ae4ff
zero.li <zeroliql@gmail.com>
2023-08-31 13:04:22 -0700

    add func declarations

--- easy-tutorial/main.go
+++ easy-tutorial/main.go
@@ -1,6 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "os"
+)
+
+func log(message string) {
+
+}
+
+// (int) or int as return, are ok
+func add(a int, b int) (int) {
+  return 1
+}
+
+func power(name string) (int, bool) {
+  return 1, false
+}
 
 func main() {
   power := 9000
@@ -9,4 +25,7 @@ func main() {
   x, power := 1, 8000  // ok, since x is new
   fmt.Println("x = ", x)
   fmt.Printf("it's over %d\n", power)
+  if len(os.Args) != 2 {
+    os.Exit(1);
+  }
 }
